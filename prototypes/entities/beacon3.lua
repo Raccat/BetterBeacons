47c7a39debd3d9eeee3131fe733e4c5819536c99
kaitlyn <87152313+kaitlynkittyy@users.noreply.github.com>
2024-02-18 17:21:13 -0500

    m

--- prototypes/entities/beacon3.lua
+++ prototypes/entities/beacon3.lua
@@ -1,3 +1,6 @@
+local sounds = require("__base__/prototypes/entity/sounds")
+local hit_effects = require ("__base__/prototypes/entity/hit-effects")
+
 data:extend({
     {
     type = "beacon",
@@ -12,7 +15,10 @@ data:extend({
         type = "electric",
         usage_priority = "secondary-input",
     },
-    minable = {mining_time = 1, result = "beacon3"},
+    corpse = "beacon-remnants",
+    dying_explosion = "beacon-explosion",
+    damaged_trigger_effect = hit_effects.entity(),
+    minable = {mining_time = 0.2, result = "beacon3"},
     supply_area_distance = "5",
     distribution_effectivity = "1",
     module_specification = {
@@ -20,17 +26,53 @@ data:extend({
             module_info_icon_shift = {0, 0},
             module_info_multi_row_initial_height_modifier = -0.3
     },
-    radius_visualisation_picture = {
-        filename = "__base__/graphics/entity/beacon/beacon-radius-visualization.png",
-        size = 10
+    radius_visualisation_picture =
+    {
+      filename = "__base__/graphics/entity/beacon/beacon-radius-visualization.png",
+      priority = "extra-high-no-scale",
+      width = 10,
+      height = 10
+    },
+    vehicle_impact_sound = sounds.generic_impact,
+    open_sound = sounds.machine_open,
+    close_sound = sounds.machine_close,
+    working_sound =
+    {
+      sound =
+      {
+        {
+          filename = "__base__/sound/beacon-1.ogg",
+          volume = 0.2
+        },
+        {
+          filename = "__base__/sound/beacon-2.ogg",
+          volume = 0.2
+        }
+      },
+      audible_distance_modifier = 0.33,
+      max_sounds_per_type = 3
     },
     allowed_effects = {"speed", "consumption", "productivity", "pollution"},
     flags = {"placeable-neutral", "player-creation"},
     selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
-    collision_box = {{-1.4, -1.4}, {1.4, 1.4}},
+    collision_box = {{-1.2, -1.2}, {1.2, 1.2}},
+    drawing_box = {{-1.5, -2.2}, {1.5, 1.3}},
     max_health = 200,
     fast_replaceable_group = "beacons",
-    -- ADD CLOSE AND OPEN SOUNDS FROM BASE GAME
-
+    water_reflection =
+    {
+      pictures =
+      {
+        filename = "__base__/graphics/entity/beacon/beacon-reflection.png",
+        priority = "extra-high",
+        width = 24,
+        height = 28,
+        shift = util.by_pixel(0, 55),
+        variation_count = 1,
+        scale = 5
+      },
+      rotate = false,
+      orientation_to_variation = false
+    }
     }
 })
---
+++ prototypes/research/effecttransmission2.lua
@@ -0,0 +1,24 @@
+local et2 = table.deepcopy(data.raw["technology"]["effect-transmission"])
+
+et2.name = "effect-transmission-2"
+et2.effects = {
+    {
+        type = "unlock-recipe",
+        recipe = "beacon2"
+    },
+}
+et2.prerequisites = {"effect-transmission"}
+et2.unit = {
+  count = 200,
+  ingredients =
+  {
+    {"automation-science-pack", 1},
+    {"logistic-science-pack", 1},
+    {"chemical-science-pack", 1},
+    {"production-science-pack", 1},
+    {"utility-science-pack", 1}
+  },
+  time = 30,
+}
+
+data:extend{et2}
