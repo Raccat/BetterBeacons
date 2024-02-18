91836aa30722f5daf21435b59ebe6a6030f02f3d
Trashpanda115 <jasminem1911@gmail.com>
2024-02-17 22:57:48 -0500

    allowed basic crafting machines to assemble beacon 2

--- recipes/beacon2.lua
+++ recipes/beacon2.lua
@@ -4,7 +4,8 @@ data:extend({
     name = "beacon2",
     enabled = true,
     energy_required = 1, -- time to craft in seconds (at crafting speed 1)
-    ingredients = {{"copper-plate", 1}, {"steel-plate", 1}},
+    ingredients = {{"beacon", 1}, {"processing-unit", 20}, {"advanced-circuit", 20}},
     result = "beacon2",
-    }
+    category = "basic-crafting",
+    }    
 })
