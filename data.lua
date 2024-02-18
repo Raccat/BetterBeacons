b5779e14547b2037a7738eaba86cfddaf7bcbe0b
kaitlyn <87152313+kaitlynkittyy@users.noreply.github.com>
2024-02-18 12:11:20 -0500

    e

--- data.lua
+++ data.lua
@@ -1,6 +1,3 @@
 require ('prototypes/items/beacon2.lua')
 require ('prototypes/entities/beacon2.lua')
 require ('recipes/beacon2.lua')
-require ('prototypes/items/beacon3.lua')
-require ('prototypes/entities/beacon3.lua')
-require ('recipes/beacon3.lua')
