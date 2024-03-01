cb982dac515d135d0def631092c08ff1cb073d2e
kaitlyn <87152313+kaitlynkittyy@users.noreply.github.com>
2024-02-29 20:51:20 -0500

    made beacons handcraftable

--- recipes/beacon2.lua
+++ recipes/beacon2.lua
@@ -6,6 +6,6 @@ data:extend({
     energy_required = 15,
     ingredients = {{"beacon", 1}, {"processing-unit", 20}, {"advanced-circuit", 20}},
     result = "beacon2",
-    category = "basic-crafting",
+    category = "crafting",
     }
 })
--- recipes/beacon3.lua
+++ recipes/beacon3.lua
@@ -6,6 +6,6 @@ data:extend({
     energy_required = 15,
     ingredients = {{"beacon2", 1}, {"productivity-module-3", 4}},
     result = "beacon3",
-    category = "basic-crafting",
+    category = "crafting",
     }
 })
